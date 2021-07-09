aaa50e9a190344db6a96950966ef5a0dfdd732f8
Omar Irfan <omar.irfan18@gmail.com>
2021-07-09 11:05:44 -0400

    stacks and queues

--- stacksAndQueues/examples.js
+++ stacksAndQueues/examples.js
@@ -68,21 +68,74 @@ class Stack {
 
 //Stack with Arrays
 
-class Stack {
+class StackWArr {
   constructor(){
     this.array = []
-    this.top = array[array.length-1];
-    this.bottom = array[0];
-    this.length = 0;
   }
   peek() {
-    return this.top
+    return this.array[this.array.length-1]
   }
   push(value){
    this.array.push(value)
+   return this
 }
   pop(){
     this.array.pop()
+    return this
   }
  
 }
+
+//Queues with LinkedList
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first
+  }
+  enqueue(value){
+    const newNode = new Node(value)
+    if(this.length === 0){
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+  }
+  this.length++
+  return this
+  }
+  dequeue(){
+    if(!this.first){
+      return null
+    }
+    if(this.first === this.last) {
+      this.last = null
+    }
+    const holdingPointer = this.first
+    this.first = this.first.next
+    this.length--
+    return this
+
+  }
+  //isEmpty;
+}
+
+const myQueue = new Queue();
+myQueue.enqueue('Joy')
+myQueue.enqueue('Matt')
+myQueue.enqueue('Pavel')
+myQueue.enqueue('Samir')
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+console.log(myQueue)
+
+//Joy
+//Matt
+//Pavel
+//Samir
